a2cfde47fcc9bfb78f7e61f30721673dc5097ac4
Dhruv Aggarwal <dhruv.2002agg@gmail.com>
2023-05-24 15:59:37 +0530

    Create 2700. Differences Between Two Objects.js

---
+++ 2700. Differences Between Two Objects.js
@@ -0,0 +1,42 @@
+/**
+ * @param {object} obj1
+ * @param {object} obj2
+ * @return {object}
+ */
+function objDiff(obj1, obj2) {
+  function isObject(val) {
+    return typeof val === 'object' && val !== null && !Array.isArray(val);
+  }
+
+  function compare(obj1, obj2, path, result) {
+    if (isObject(obj1) && isObject(obj2)) {
+      for (const key in obj1) {
+        if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
+          compare(obj1[key], obj2[key], [...path, key], result);
+        }
+      }
+    } else if (Array.isArray(obj1) && Array.isArray(obj2)) {
+      const maxLength = Math.max(obj1.length, obj2.length);
+      for (let i = 0; i < maxLength; i++) {
+        if (i in obj1 && i in obj2) {
+          compare(obj1[i], obj2[i], [...path, i], result);
+        }
+      }
+    } else if (obj1 !== obj2) {
+      let currentObj = result;
+      for (let i = 0; i < path.length - 1; i++) {
+        const key = path[i];
+        if (!(key in currentObj)) {
+          currentObj[key] = {};
+        }
+        currentObj = currentObj[key];
+      }
+      const lastKey = path[path.length - 1];
+      currentObj[lastKey] = [obj1, obj2];
+    }
+  }
+
+  const result = {};
+  compare(obj1, obj2, [], result);
+  return result;
+}
